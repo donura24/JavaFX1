700e4b1cedea8a88fa1668b18f6d8707a7545efb
donura24 <teodorpenchev24@gmail.com>
2023-04-07 19:46:14 +0300

    fc

--- src/main/java/com/example/javafx1/HelloApplication.java
+++ src/main/java/com/example/javafx1/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -15,11 +16,20 @@ public class HelloApplication extends Application {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Group root = new Group();
-        Scene scene = new Scene(root, Color.BEIGE);
+        Scene scene = new Scene(root, Color.BISQUE);
         Image icon = new Image("Mann-Mit-Der-Tarnjacke-Aurora-Monitor.32.png");
 
         stage.getIcons().add(icon);
         stage.setTitle("First App!");
+        stage.setWidth(400);
+        stage.setHeight(400);
+        //stage.setResizable(false);
+
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Press SPACE to exit FullScreen.");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("SPACE"));
+        //stage.setX();
+        //stage.setY();
         stage.setScene(scene);
         stage.show();
     }
