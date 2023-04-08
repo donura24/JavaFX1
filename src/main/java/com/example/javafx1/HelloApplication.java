30008ca403a1bd35dbb50caf4573652abbe78029
donura24 <teodorpenchev24@gmail.com>
2023-04-08 17:29:48 +0300

    fc

--- src/main/java/com/example/javafx1/HelloApplication.java
+++ src/main/java/com/example/javafx1/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,9 +17,16 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
         Group root = new Group();
-        Scene scene = new Scene(root, Color.BISQUE);
+        Scene scene = new Scene(root, Color.DARKSEAGREEN);
         Image icon = new Image("Mann-Mit-Der-Tarnjacke-Aurora-Monitor.32.png");
+        Text text = new Text("Paradiseee!");
+
+        text.setX(100);
+        text.setY(100);
+        text.setFont(Font.font("Ubuntu", 40));
+        text.setFill(Color.AZURE);
 
         stage.getIcons().add(icon);
         stage.setTitle("First App!");
@@ -25,11 +34,12 @@ public class HelloApplication extends Application {
         stage.setHeight(400);
         //stage.setResizable(false);
 
-        stage.setFullScreen(true);
-        stage.setFullScreenExitHint("Press SPACE to exit FullScreen.");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("SPACE"));
+        //stage.setFullScreen(true);
+        //stage.setFullScreenExitHint("Press SPACE to exit FullScreen.");
+        //stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("SPACE"));
         //stage.setX();
         //stage.setY();
+        root.getChildren().add(text);
         stage.setScene(scene);
         stage.show();
     }
