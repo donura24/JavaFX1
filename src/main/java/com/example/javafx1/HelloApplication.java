27c1a1da2491968aab1eb2ad097f864ecc9a71bd
donura24 <teodorpenchev24@gmail.com>
2023-04-08 18:08:44 +0300

    fc

--- src/main/java/com/example/javafx1/HelloApplication.java
+++ src/main/java/com/example/javafx1/HelloApplication.java
@@ -4,8 +4,13 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -21,12 +26,51 @@ public class HelloApplication extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, Color.DARKSEAGREEN);
         Image icon = new Image("Mann-Mit-Der-Tarnjacke-Aurora-Monitor.32.png");
-        Text text = new Text("Paradiseee!");
 
+        Text text = new Text("Paradiseee!");
         text.setX(100);
         text.setY(100);
         text.setFont(Font.font("Ubuntu", 40));
         text.setFill(Color.AZURE);
+
+        Line line = new Line();
+        line.setStartX(0);
+        line.setStartY(200);
+        line.setEndX(400);
+        line.setEndY(200);
+        line.setStrokeWidth(3);
+        line.setStroke(Color.DEEPPINK);
+        line.setOpacity(0.5);
+        //line.setRotate(45);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(100);
+        rectangle.setY(50);
+        rectangle.setWidth(210);
+        rectangle.setHeight(80);
+        rectangle.setStrokeWidth(4);
+        rectangle.setStroke(Color.BISQUE);
+        rectangle.setFill(Color.DARKSLATEBLUE);
+
+        Polygon polygon = new Polygon();
+        polygon.getPoints().setAll(
+                200.0,200.0,
+                300.0, 300.0,
+                200.0,300.0
+                );
+        polygon.setFill(Color.IVORY);
+        polygon.setRotate(120);
+
+        Circle circle = new Circle();
+        circle.setCenterX(350);
+        circle.setCenterY(350);
+        circle.setRadius(50);
+        circle.setFill(Color.VIOLET);
+
+        Image image = new Image("Icondrawer-Globe-Globe-compass.128.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setX(50);
+        imageView.setY(150);
 
         stage.getIcons().add(icon);
         stage.setTitle("First App!");
@@ -39,7 +83,13 @@ public class HelloApplication extends Application {
         //stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("SPACE"));
         //stage.setX();
         //stage.setY();
+
+        root.getChildren().add(rectangle);
         root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(polygon);
+        root.getChildren().add(circle);
+        root.getChildren().add(imageView);
         stage.setScene(scene);
         stage.show();
     }
