38b9e3e80d2932677a2b2de9dc111eb59628e3ba
donura24 <teodorpenchev24@gmail.com>
2023-04-09 18:11:10 +0300

    fc

--- src/main/java/com/example/javafx1/HelloApplication.java
+++ src/main/java/com/example/javafx1/HelloApplication.java
@@ -1,7 +1,9 @@
 package com.example.javafx1;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -22,6 +24,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Parent parent = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
 
         Group root = new Group();
         Scene scene = new Scene(root, Color.DARKSEAGREEN);
