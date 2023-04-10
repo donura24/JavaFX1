37e3c7a84d609152a4b93bba245e70ef01295152
donura24 <teodorpenchev24@gmail.com>
2023-04-10 22:55:52 +0300

    fc

--- src/main/java/CircleMover/Controller.java
+++ src/main/java/CircleMover/Controller.java
@@ -1,24 +1,32 @@
 package CircleMover;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Sphere;
 
 public class Controller {
 
-    Circle circle;
+    @FXML
+    private Circle circle;
+    private double x;
+    private double y;
+
 
     public void up(ActionEvent e){
-
-        System.out.println("up");
+        circle.setCenterY(y-=5);
     }
     public void down(ActionEvent e){
 
+        circle.setCenterY(y+=5);
     }
     public void right(ActionEvent e){
 
+        circle.setCenterX(x+=5);
     }
     public void left(ActionEvent e){
 
+        circle.setCenterX(x-=5);
     }
 
 }
