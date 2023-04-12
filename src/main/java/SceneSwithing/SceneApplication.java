0a5a9eb9e999f542a8c4e5c0395b0faedab5ff37
donura24 <teodorpenchev24@gmail.com>
2023-04-12 23:22:55 +0300

    fc

--- src/main/java/SceneSwithing/SceneApplication.java
+++ src/main/java/SceneSwithing/SceneApplication.java
@@ -2,8 +2,12 @@ package SceneSwithing;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class SceneApplication extends Application {
@@ -14,8 +18,17 @@ public class SceneApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+
         Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
         Scene scene1 = new Scene(root);
+        Image im = new Image("Dark-Wizard-1-icon.png");
+        ImageView imageView = new ImageView(im);
+        imageView.setX(50);
+        imageView.setY(100);
+
+
+        ((Pane) root).getChildren().add(imageView);
+
         stage.setScene(scene1);
         stage.show();
     }
--- src/main/java/SceneSwithing/SceneController.java
+++ src/main/java/SceneSwithing/SceneController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +23,13 @@ public class SceneController {
         root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+        Image im = new Image("Dark-Wizard-1-icon.png");
+        ImageView imageView = new ImageView(im);
+        imageView.setX(50);
+        imageView.setY(100);
+
+
+        ((Pane) root).getChildren().add(imageView);
         stage.setScene(scene);
         stage.show();
     }
@@ -28,6 +38,13 @@ public class SceneController {
         root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+        Image im = new Image("Castle-Princess-icon.png");
+        ImageView imageView = new ImageView(im);
+        imageView.setX(50);
+        imageView.setY(100);
+
+
+        ((Pane) root).getChildren().add(imageView);
         stage.setScene(scene);
         stage.show();
 
