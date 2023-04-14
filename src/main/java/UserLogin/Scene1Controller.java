dbdde622784702fdb08c25050cd7a201346178a2
donura24 <teodorpenchev24@gmail.com>
2023-04-14 23:23:20 +0300

    fc

--- src/main/java/UserLogin/Scene1Controller.java
+++ src/main/java/UserLogin/Scene1Controller.java
@@ -1,11 +1,15 @@
 package UserLogin;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,8 +25,11 @@ public class Scene1Controller {
     private Stage stage;
     private Scene scene;
     private Parent parent;
+    @FXML
+    private ProgressBar progressBar;
 
-    public void login(ActionEvent event) throws IOException {
+    public void login(ActionEvent event) throws IOException, InterruptedException {
+
         String username = textField.getText();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
