2f5bb16436078717f436df12bc32d8549d380067
donura24 <teodorpenchev24@gmail.com>
2023-04-15 19:09:59 +0300

    fc

--- src/main/java/LogoutWindow/AppController.java
+++ src/main/java/LogoutWindow/AppController.java
@@ -1,7 +1,25 @@
 package LogoutWindow;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class AppController {
 
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private AnchorPane scenePane;
+    Stage stage;
+
+    public void logout(ActionEvent event){
+
+        stage = (Stage) scenePane.getScene().getWindow();
+        System.out.println("okk");
+        stage.close();
+    }
 }
