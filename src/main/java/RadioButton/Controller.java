7f4fff3314b493d110ec1210d82e2dc53eb1054d
donura24 <teodorpenchev24@gmail.com>
2023-04-18 23:25:56 +0300

    fc

--- src/main/java/RadioButton/Controller.java
+++ src/main/java/RadioButton/Controller.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class Controller {
 
@@ -11,14 +14,30 @@ public class Controller {
     private Label myLabel;
     @FXML
     private RadioButton myRadioButton1, myRadioButton2, myRadioButton3;
+    @FXML
 
-    public void selected(ActionEvent event){
+    Image myCarIcon1 = new Image("icons8-subaru-96.png");
+    Image myCarIcon2 = new Image("icons8-toyota-96.png");
+    Image myCarIcon3 = new Image("icons8-honda-96.png");
 
-        if (myRadioButton1.isSelected()){
+
+    @FXML
+    ImageView myCarIcon = new ImageView(myCarIcon1);
+
+    public void selected(ActionEvent event) {
+
+
+        if (myRadioButton1.isSelected()) {
             myLabel.setText(myRadioButton1.getText());
+            myCarIcon.setImage(myCarIcon1);
         } else if (myRadioButton2.isSelected()) {
             myLabel.setText(myRadioButton2.getText());
+            myCarIcon.setImage(myCarIcon2);
+
+        } else {
+            myLabel.setText(myRadioButton3.getText());
+            myCarIcon.setImage(myCarIcon3);
+
         }
-        else myLabel.setText(myRadioButton3.getText());
     }
 }
