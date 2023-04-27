d69711f92d30ce6c5f85e071aeea403540cdf830
donura24 <teodorpenchev24@gmail.com>
2023-04-28 00:01:00 +0300

    fc

--- src/main/java/MediaVieww/Controller.java
+++ src/main/java/MediaVieww/Controller.java
@@ -1,5 +1,45 @@
 package MediaVieww;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private MediaView myMediaView;
+
+    @FXML
+    private Button myPlayButton, myPauseButton, myResetButton;
+
+    private File file;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        file = new File("Thirsty.mp4");
+        media = new Media(file.toURI().toString());
+
+    }
+
+    public void playMedia(){
+
+    }
+    public void pauseMedia(){
+
+    }
+    public void resetMedia(){
+
+    }
 }
