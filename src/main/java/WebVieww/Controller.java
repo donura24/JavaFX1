a94712b2d8bc8613f59c130dd9528cfc3a50ef6e
donura24 <teodorpenchev24@gmail.com>
2023-04-29 17:59:59 +0300

    fc

--- src/main/java/WebVieww/Controller.java
+++ src/main/java/WebVieww/Controller.java
@@ -1,5 +1,32 @@
 package WebVieww;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private WebView myWebView;
+
+    @FXML
+    private TextField myTextField;
+
+    private WebEngine webEngine;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        webEngine = myWebView.getEngine();
+        loadPage();
+    }
+
+    public void loadPage(){
+
+        webEngine.load("http://"+myTextField.getText());
+    }
 }
