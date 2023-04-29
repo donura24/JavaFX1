fd37b28d866c061b112a3f669da96e10de404c75
donura24 <teodorpenchev24@gmail.com>
2023-04-29 18:08:42 +0300

    fc

--- src/main/java/WebVieww/Controller.java
+++ src/main/java/WebVieww/Controller.java
@@ -18,15 +18,35 @@ public class Controller implements Initializable {
 
     private WebEngine webEngine;
 
+    private String homePage;
+
+    private double webZoom;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         webEngine = myWebView.getEngine();
+        homePage = "www.google.com";
+        myTextField.setText(homePage);
+        webZoom = 1.0;
         loadPage();
     }
 
     public void loadPage(){
 
         webEngine.load("http://"+myTextField.getText());
+    }
+
+    public void refreshPage(){
+        webEngine.reload();
+    }
+
+    public void zoomIn(){
+        webZoom+=0.25;
+        myWebView.setZoom(webZoom);
+    }
+    public void zoomOut(){
+        webZoom-=0.25;
+        myWebView.setZoom(webZoom);
     }
 }
