5235ada9986777df59bf1b1d00685c07dee328ad
donura24 <teodorpenchev24@gmail.com>
2023-05-17 23:13:01 +0300

    fc

--- src/main/java/webBrowser/Controller.java
+++ src/main/java/webBrowser/Controller.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+//
 
 import javafx.scene.layout.*;
 import javafx.scene.web.WebEngine;
